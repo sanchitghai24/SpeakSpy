96c8cfd790331ddd3307825a5e45e0e91f432afc
sanchit <sanchitghai24@gmail.com>
2017-09-30 23:03:26 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,3 @@
+spy_name = raw_input("Hello Spyy...What's your name ??\n")
+spy_salutation = raw_input("What should our community call you ? Mr. , Ms. or Mrs. ?\n")
+print "Alright "+ spy_salutation + "." + spy_name + " . We would like to know a little more about.."
