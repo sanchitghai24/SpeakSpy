fc3b6c63ba351102a262bb02615604afd566eaf5
sanchit <sanchitghai24@gmail.com>
2017-10-01 15:37:58 +0530

    third

--- main.py
+++ main.py
@@ -1,38 +1,97 @@
+def add_status(present_status):
+    if present_status!=None:
+        print "Your present Status is "+ present_status+"\n"
+    else:
+        print "You don't have any present status.."
 
-spy_name = raw_input("Hello Spyy...What's your name ??\n")
-spy_salutation = raw_input("What should our community call you ? Mr. , Ms. or Mrs. ?\n")
+    status_default = ["Rishte me to hum tumahre baap lagte hai..","Hum tumahri aukat se bahar hai..","Wanna be the king of spy..Follow me ;)"]
+    default = raw_input ("Wanna select from default status ?? (Y/N)\n")
 
-if len(spy_name)and len(spy_salutation)>0:
-    print "Alright "+ spy_salutation + "." + spy_name + " . We would like to know a little more about.."
+    if default.upper() == "n" or default.upper() == "N":
+        new_status = raw_input ("Please type your status")
+
+        if len(new_status)>0:
+            updated_status = new_status
+            status_default.append(updated_status)
+
+    elif default.upper() == "Y" or default.upper() == "y":
+        item_position = 1
+        for message in status_default:
+            print str(item_position) + ". " + message
+            item_position = item_position + 1
+
+        message_selection = input("\n Choose from the above status..")
+        if len(status_default)>= message_selection:
+            updated_status = status_default[message_selection - 1]
+
+    return updated_status
+
+def start_chat(spy_name,spy_age,spy_rating):
+    menu_show = True
+    present_status = None
+
+    while menu_show:
+        print "MENU\n 1.Add a Status Update\n 2.Add a Friend\n 3.Send a secret message\n 4.Read a secret message\n 5.Read Chats from a User\n 6.Close Application"
+        menu_choice = input("Your Choice: ")
+
+        if menu_choice == 1:
+            print "You choose Update Status Option.."
+            present_status = add_status(present_status)
+            print "Status Updated to : "+ present_status
+        elif menu_choice == 2:
+            print "You choose Add a friend Option.."
+        elif menu_choice ==3:
+            print "You choose Send a secret message Option.."
+        elif menu_choice ==4:
+            print "You choose Read a secret message Option.."
+        elif menu_choice == 5:
+            print "You choose Read Chats Option.."
+        else:
+            print "You choose Close Application..\n Thank You for your visit !!"
+            exit()
+
+
+from spy_details import spy_name, spy_salutation, spy_age, spy_rating
+
+question = "Continue as "+spy_salutation+"."+spy_name+" (Y/N)?\n"
+user = raw_input(question)
+
+if user == "Y" or user == "y":
+    print ""
 else:
-    print "Please fill the Name and Salutation fields.."
+    spy_name = raw_input("Hello Spyy...What's your name ??\n")
+    spy_salutation = raw_input("What should our community call you ? Mr. , Ms. or Mrs. ?\n")
 
-spy_age = 0
-spy_rating = 0.0
-spy_is_online = False
+    if len(spy_name)and len(spy_salutation)>0:
+        print "Alright "+ spy_salutation + "." + spy_name + " . We would like to know a little more about.."
+    else:
+        print "Fields not filled correctly..Please try again.."
+        exit()
 
-spy_age = input("What's your age ??\n")
+    spy_age = input("What's your age ??\n")
+    spy_age = int(spy_age)
 
-if spy_age > 50:
-    print "OH! Grandpa Spy is here :P "
-elif spy_age <50 and spy_age >12:
-    print "Young Spy is here..WELCOME "
-else:
-    print "Not Eligible kiddo..Go Home and try again after some years ! "
-    exit()
+    if spy_age > 50:
+         print "OH! Grandpa Spy is here :P "
+    elif spy_age <50 and spy_age >12:
+        print "Young Spy is here..WELCOME "
+    else:
+        print "Not Eligible kiddo..Go Home and try again after some years ! "
+        exit()
 
-spy_rating = input("What's your rating??\n")
+    spy_rating = input("What's your rating??\n")
+    spy_rating = float(spy_rating)
 
-if spy_rating>=4.5:
-    print "Hello Undercover Agent Spy.."
-elif spy_rating<4.5 and spy_rating>=3.5:
-    print "Hello Foreign Agent Spy"
-elif spy_rating<3.5 and spy_rating>=2.5:
-    print "Hello Double Agent Spy"
-elif spy_rating<2.5 and spy_rating>=1.5:
-    print "Hello Secret Service Agent Spy"
-else:
-    print "I Hope you are promoted soon. Keep up"
+    if spy_rating>=4.5:
+        print "Hello Undercover Agent Spy.."
+    elif spy_rating<4.5 and spy_rating>=3.5:
+        print "Hello Foreign Agent Spy"
+    elif spy_rating<3.5 and spy_rating>=2.5:
+        print "Hello Double Agent Spy"
+    elif spy_rating<2.5 and spy_rating>=1.5:
+        print "Hello Secret Service Agent Spy"
+    else:
+        print "I Hope you are promoted soon. Keep up"
 
 print "------------------------------------------------"
 
@@ -40,4 +99,6 @@ spy_is_online = True
 
 print "Authentication Complete! Now you are entering the SpySpeak Zone.."
 print "Welcome "+spy_salutation+"."+spy_name+", Age:"+str(spy_age)+", Rating:"+str(spy_rating)+" . Proud to serve you."
+
+start_chat(spy_name,spy_age,spy_rating)
 
