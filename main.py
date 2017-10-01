dd07970db67526ff7fcedc6d77075d8998095597
sanchit <sanchitghai24@gmail.com>
2017-10-01 19:47:25 +0530

    fourth

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
+from spy_details import spy_name, spy_salutation, spy_age, spy_rating
+
+status_default = ["Rishte me to hum tumahre baap lagte hai..", "Hum tumahri aukat se bahar hai..","Wanna be the king of spy..Follow me ;)"]
+spy_friend_name=[]
+spy_friend_age=[]
+spy_friend_rating=[]
+spy_friend_is_online=[]
+
+def add_friend():
+    new_name = input("Add your Co-Spy's Name\n")
+    new_salutation = input("Are they Mr or Ms ? : ")
+    new_name = new_salutation + "." + new_name
+    new_age = input("Age?\n")
+    new_rating = input("Spy rating?\n")
+
+    if len(new_name) > 0 and new_age > 12 and new_rating >= spy_rating:
+        spy_friend_name.append(new_name)
+        spy_friend_age.append(new_age)
+        spy_friend_rating.append(new_rating)
+        spy_friend_is_online.append(True)
+    else:
+        print "Sorry..The Spy you entered does not exist"
+
+    return len(spy_friend_name)
+
 def add_status(present_status):
     if present_status!=None:
         print "Your present Status is "+ present_status+"\n"
     else:
         print "You don't have any present status.."
 
-    status_default = ["Rishte me to hum tumahre baap lagte hai..","Hum tumahri aukat se bahar hai..","Wanna be the king of spy..Follow me ;)"]
     default = raw_input ("Wanna select from default status ?? (Y/N)\n")
 
-    if default.upper() == "n" or default.upper() == "N":
-        new_status = raw_input ("Please type your status")
+    if default.upper() == "N":
+        new_status = raw_input ("Please type your status\n")
 
         if len(new_status)>0:
             updated_status = new_status
             status_default.append(updated_status)
 
-    elif default.upper() == "Y" or default.upper() == "y":
+    elif default.upper() == "Y":
         item_position = 1
         for message in status_default:
             print str(item_position) + ". " + message
@@ -51,12 +75,10 @@ def start_chat(spy_name,spy_age,spy_rating):
             exit()
 
 
-from spy_details import spy_name, spy_salutation, spy_age, spy_rating
-
 question = "Continue as "+spy_salutation+"."+spy_name+" (Y/N)?\n"
 user = raw_input(question)
 
-if user == "Y" or user == "y":
+if user.upper() == "Y":
     print ""
 else:
     spy_name = raw_input("Hello Spyy...What's your name ??\n")
